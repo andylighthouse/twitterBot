ccfff9b760c55b155274c9e81c3baee0257a6b87
Andy Li <andylibc@gmail.com>
2017-05-11 11:31:12 -0700

    working with get and post

--- index.js
+++ index.js
@@ -9,12 +9,81 @@ var bot = new Twit({
 });
 
 
-bot.post('statuses/update', {status: 'hello usa..!!dfdf!S'}, function(err, data, response){
-  if(err){
-    console.log(err);
-  }else{
-    console.log(data.text + ' was tweeted');
-  }
-});
+// //making a twit, put message in status
+// bot.post('statuses/update', {status: 'Hello World, testing!'}, function(err, data, response){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(data.text + ' was tweeted');
+//   }
+// });
 
+// //screen_name: twitter handle
+// //check users that are following me
+// bot.get('followers/list', {secreen_name: 'sugacity99'}, function(err, data, response){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(data);
+//   }
+// });
+
+
+
+// //make bot follow someone
+// bot.post('friendships/create', {screen_name: 'TWITTER_HANDLE'}, function(err, data, response){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(data);
+//   }
+// });
+
+// //check users that the bot is following
+// bot.get('friends/list', {screen_name: 'sugacity99'}, function(err, data, response){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(data);
+//   }
+// });
+
+// //lookup relationship
+// bot.get('friendships/lookup', {screen_name: 'GreenChecked'}, function(err, data, response){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(data);
+//   }
+// });
+
+//send direct message to follower
+// bot.post('direct_messages/new', {screen_name: 'GreenChecked', 
+//   text: "hello there!, testing from my bot, u little hippie"}, 
+//   function(err, data, response){
+//     if(err){
+//       console.log(err);
+//     }else{
+//       console.log(data);
+//     }
+// });
+
+//get most recent twits, limited by count
+function getTwits(){
+  bot.get('statuses/home_timeline', {count: 5}, 
+    function(err, data, response){
+    if(err){
+        console.log(err);
+      }else{
+        data.forEach(function(twit){
+          console.log(twit.text);
+          console.log(twit.user.screen_name);
+          console.log(twit.id_str);
+          console.log('\n');
+      })
+    }
+  })
+}
+
+getTwits();
 
