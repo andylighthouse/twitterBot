594f421c71df3acafbece0d296defa3cf973fa5e
Andy Li <andylibc@gmail.com>
2017-06-16 01:17:45 -0700

    finished rita

--- rita.js
+++ rita.js
@@ -3,12 +3,53 @@ var fs = require('fs');
 var csvparse = require('csv-parse');
 var rita = require('rita');
 
+var filePath = './past_tweets/tweets.csv'
+var inputText = "ANY TEXT"
 
-var bot = new Twit({
-  consumer_key: process.env.TWITTERBOT_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTERBOT_CONSUMER_SECRET,
-  access_token: process.env.TWITTERBOT_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTERBOT_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60000
-})
+var tweetData = fs.createReadStream(filePath)
+  .pipe(csvparse({delimiter: ','}))
+  .on('data', function(row){
+    inputText = inputText + '' + cleanText(row[5])
+  })
+  .on('end', function(){
+    var markov = new rita.RiMarkov(3)
+    markov.loadText(inputText)
+    var sentence = markov.generateSentences(1)
+    bot.post('statuses/update', {status: sentence}, function(err, data, response){
+      if(err){
+        console.log(err)
+      }else{
+        console.log('status tweeted')
+      }
+    })
+  })
 
+function hasNoStopWords(token){
+  var stopwords = ['@', 'http', 'RT']
+  return stopwords.every(function(stopword){
+    return !token.includes(stopword)
+  })
+}
+
+function cleanText(text){
+  //break up the tweet into tokens, but space
+  return rita.RiTa.tokenize(text, ' ')
+  .filter(hasNoStopWords)
+  .join(' ')
+  .trim()
+}
+
+
+
+// //using markov chain
+// var markov = new rita.RiMarkov(3)
+// //load the sample text
+// markov.loadText(inputText)
+// var sentences = markov.generateSentences(1)
+// console.log(sentences)
+
+// //probability of showing the word
+// console.log(markov.getProbability("Trump"))
+
+// //probability of getting the next word after "Trump"
+// console.log(markov.getProbabilities("Trump"))
